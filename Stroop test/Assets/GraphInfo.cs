a3f4048413666f41085747dcaa27f553d178f2f2
V <hanimohammad00@gmail.com>
2021-11-11 16:04:03 +0400

    Graph animated

--- Stroop test/Assets/GraphInfo.cs
+++ Stroop test/Assets/GraphInfo.cs
@@ -13,38 +13,38 @@ public class GraphInfo : MonoBehaviour
     public TextMeshProUGUI averageTimeText;
     public TextMeshProUGUI mistakesText;
     public TextMeshProUGUI difficultyText;
+    private List<BarInfo> barsInfo;
     private List<float> questionTimes;
+    private List<float> heightValues;
 
     public void UpdateBars(List<BarInfo> infoBar, int mistakes, string difficulty) // Could Animate with Lerp or move towards
     {
         questionTimes = new List<float>();
+        barsInfo = infoBar;
         foreach (var bar in infoBar)
-        {
             questionTimes.Add(bar.GetTime());
-        }
         UpdateText(mistakes, difficulty);
-        UpdateBarsHeight(infoBar);
+        UpdateBarsHeight();
     }
 
-    void UpdateBarsHeight(List<BarInfo> infoBar)
+    void UpdateBarsHeight()
     {
+        heightValues = new List<float>();
         float highestNumber = UpdateTimeLabels();
         float oneHeight = 380/highestNumber;
         
-        for (int i = 0; i < infoBar.Count; i++)
+        for (int i = 0; i < bars.Length; i++)
         {
             //Set the sze and calculate height
-            RectTransform barImage = bars[i].GetChild(0).GetComponent<RectTransform>();
-            RectTransform barText = bars[i].GetChild(1).GetComponent<RectTransform>();
+            Transform barImage = bars[i].GetChild(0);
+            Transform barText = barImage.GetChild(0);
             
             float seconds = questionTimes[i];
             float oneDecimal = Mathf.Round(seconds * 10) / 10;
             float value = seconds * oneHeight;
+            heightValues.Add(value);
             
-            barImage.sizeDelta = new Vector2(20, value); // Animate this
-            barText.sizeDelta = new Vector2(value, 20); //       Ani
-            
-            if (infoBar[i].GetCorrect()) // Set the colors and numbers in the Text
+            if (barsInfo[i].GetCorrect()) // Set the colors and numbers in the Text
             {
                 barImage.GetComponent<Image>().color = new Color32(26,128,254,255);
                 if(value >50)
@@ -78,4 +78,18 @@ public class GraphInfo : MonoBehaviour
         return highestInt;
     }
 
+    private void Update()
+    {
+        if (gameObject.activeSelf)
+        {
+            for (int i = 0; i < bars.Length; i++)
+            {
+                RectTransform barImage = bars[i].GetChild(0).GetComponent<RectTransform>();
+                Vector2 targetSize = new Vector2(20, heightValues[i]);
+                barImage.sizeDelta = Vector2.MoveTowards(barImage.sizeDelta, new Vector2(20, heightValues[i]), .5f);
+                //move towards moves in a certain amount of time
+                //Lerp uses Time delta
+            }
+        }
+    }
 }
