54b0b532432261ee10acf34e861672bc86222880
V <hanimohammad00@gmail.com>
2021-11-11 19:31:02 +0400

    Numbers Animate + timedelta added

--- Stroop test/Assets/GraphInfo.cs
+++ Stroop test/Assets/GraphInfo.cs
@@ -16,50 +16,33 @@ public class GraphInfo : MonoBehaviour
     private List<BarInfo> barsInfo;
     private List<float> questionTimes;
     private List<float> heightValues;
+    private List<float> tempNumbers;
 
     public void UpdateBars(List<BarInfo> infoBar, int mistakes, string difficulty) // Could Animate with Lerp or move towards
     {
         questionTimes = new List<float>();
         barsInfo = infoBar;
-        foreach (var bar in infoBar)
-            questionTimes.Add(bar.GetTime());
+        CalculateHeights();
         UpdateText(mistakes, difficulty);
-        UpdateBarsHeight();
+        tempNumbers = new List<float>() {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
     }
 
-    void UpdateBarsHeight()
+    void CalculateHeights()
     {
         heightValues = new List<float>();
-        float highestNumber = UpdateTimeLabels();
-        float oneHeight = 380/highestNumber;
         
-        for (int i = 0; i < bars.Length; i++)
+        foreach (var bar in barsInfo)
+            questionTimes.Add(bar.GetTime());
+        
+        float highestNumber = UpdateTimeLabels();
+        float normalizedHeight = 380/highestNumber;
+        
+        foreach (var time in questionTimes)
         {
-            //Set the sze and calculate height
-            Transform barImage = bars[i].GetChild(0);
-            Transform barText = barImage.GetChild(0);
-            
-            float seconds = questionTimes[i];
-            float oneDecimal = Mathf.Round(seconds * 10) / 10;
-            float value = seconds * oneHeight;
+            float value = time * normalizedHeight;
             heightValues.Add(value);
-            
-            if (barsInfo[i].GetCorrect()) // Set the colors and numbers in the Text
-            {
-                barImage.GetComponent<Image>().color = new Color32(26,128,254,255);
-                if(value >50)
-                    barText.GetComponent<TextMeshProUGUI>().text = "" + seconds;
-                else
-                    barText.GetComponent<TextMeshProUGUI>().text = "" + oneDecimal;
-            }
-            else
-            {
-                barImage.GetComponent<Image>().color = new Color32(191, 76, 76,255);
-                barText.GetComponent<TextMeshProUGUI>().text = "X";
-            }
         }
     }
-
     void UpdateText(int mistakes, string mode)
     {
         float average = Mathf.Round(questionTimes.Average()*100)/100;
@@ -82,14 +65,55 @@ public class GraphInfo : MonoBehaviour
     {
         if (gameObject.activeSelf)
         {
-            for (int i = 0; i < bars.Length; i++)
+            UpdateBarNumbers();
+            GrowBars();
+        }
+    }
+
+    private void UpdateBarNumbers()
+    {
+        for (int i = 0; i < barsInfo.Count; i++)
+        {
+            float value = heightValues[i];
+            float seconds = questionTimes[i];
+            float oneDecimal = Mathf.Round(seconds * 10) / 10;
+            
+            
+            Transform barImage = bars[i].GetChild(0);
+            Transform barText = barImage.GetChild(0);
+
+            if (barsInfo[i].GetCorrect()) // Set the colors and numbers in the Text
             {
-                RectTransform barImage = bars[i].GetChild(0).GetComponent<RectTransform>();
-                Vector2 targetSize = new Vector2(20, heightValues[i]);
-                barImage.sizeDelta = Vector2.MoveTowards(barImage.sizeDelta, new Vector2(20, heightValues[i]), .5f);
-                //move towards moves in a certain amount of time
-                //Lerp uses Time delta
+                barImage.GetComponent<Image>().color = new Color32(26,128,254,255);
+                tempNumbers[i] = Mathf.MoveTowards(tempNumbers[i], seconds,
+                      Time.deltaTime/seconds * .7f);
+                if (value > 50)
+                {
+                    float second = Mathf.Round(tempNumbers[i] * 100) / 100;
+                    barText.GetComponent<TextMeshProUGUI>().text = "" + second;
+                }
+                else
+                {
+                    float secondOneDecimal = Mathf.Round(tempNumbers[i] * 10) / 10;
+                    barText.GetComponent<TextMeshProUGUI>().text = "" + secondOneDecimal;
+                }
+
             }
+            else
+            {
+                barImage.GetComponent<Image>().color = new Color32(191, 76, 76,255);
+                barText.GetComponent<TextMeshProUGUI>().text = "X";
+            }
+        }
+    }
+
+    private void GrowBars()
+    {
+        for (int i = 0; i < bars.Length; i++)
+        {
+            RectTransform barImage = bars[i].GetChild(0).GetComponent<RectTransform>();
+            Vector2 targetSize = new Vector2(20, heightValues[i]);
+            barImage.sizeDelta = Vector2.MoveTowards(barImage.sizeDelta, targetSize, Time.deltaTime * 70f);
         }
     }
 }
